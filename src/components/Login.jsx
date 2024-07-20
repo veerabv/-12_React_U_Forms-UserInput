7892d8929ca95eeaf3b7215360f88708ae60b971
veerabv <bveera0608@gmail.com>
2024-07-20 16:02:04 +0530

    generic getting form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
+
 export default function Login() {
+  const[enteredValue , setEntereValue] = useState({email:"",password:""});
+  // const[enteredPassword , setEnteredPassword] = useState("");
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log(enteredValue);
+  }
+
+  function handleChange(e,identifier){
+    setEntereValue((pv) => ({...pv,[identifier]:e.target.value}))
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(e) => handleChange(e,"email")} value={enteredValue.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password"  onChange={(e) => handleChange(e,"password")} value={enteredValue.password}/>
         </div>
       </div>
 
